796cc22a879ba1764b43d75614ec950680c60729
erfanullahkhan71-afk <erfanullahkhan71@gmail.com>
2025-10-30 11:01:04 +0430

    Updated project files

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,7 @@
-
 import Nave from "./component/nav/Nave";
 import Herosection from "./component/Hero/Herosection";
 import Lern3 from "./component/Lern3/Lern3";
-import WeBring4 from "./component/WeBraing4/WeBring4";
+
 import Down from "./component/whatOurStudetsSay/down";
 import Footer from "./component/footer/Footer";
 import Courses from "./component/cardes/Courses";
@@ -13,12 +12,11 @@ function app() {
     <>
       <Nave />
       <Herosection />
-      <Courses/>
+      <Courses />
       <Lern3 />
-      <WeBring4/>
-<Studentsay/>
-      <Down/>
-      <Footer/>
+  <Studentsay/>
+      <Down />
+      <Footer />
     </>
   );
 }
